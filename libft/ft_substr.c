fd00be673c05b9e3403acc3c232df69d46269714
Chloekkk <skdusdl8804@gmail.com>
2021-05-07 16:50:19 +0900

    add : substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,14 +6,27 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 21:35:06 by nkim              #+#    #+#             */
-/*   Updated: 2021/05/05 22:12:17 by nkim             ###   ########.fr       */
+/*   Updated: 2021/05/07 15:56:54 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
-// #include <stdlib.h>
+#include "libft.h"
+#include <stdlib.h>
 
-// char *ft_substr(char const *s, unsigned int start, size_t len)
-// {
-//     return 0;
-// }
+char *ft_substr(char const *str, unsigned int start, size_t len)
+{
+    char *tmp;
+    size_t i;
+    
+    if(!(tmp = malloc(len + 1)))
+        return 0;
+    i = 0;
+    
+    while (i < len)
+    {
+        tmp[i] = str[start + i];
+        i++;
+    }
+    tmp[i] ='\0';
+    return tmp;
+}
